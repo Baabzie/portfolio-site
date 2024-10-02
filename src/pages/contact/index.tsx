2e0cbeab247b01c036b76647c7fb76f7518fd439
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2024-10-02 10:57:24 +0200

    some styling and contacts

---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Contact: React.FC = () => {
+  return (
+    <address>
+      <p>
+        <a href="tel:+46793373714">+46 79 337 37 14</a>
+      </p>
+      <p>
+        <a href="mailto:tobiasolausson91@gmail.com">
+          tobiasolausson91@gmail.com
+        </a>
+      </p>
+    </address>
+  );
+};
+
+export default Contact;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,21 @@
-const Portfolio: React.FC = () => {
+const Home: React.FC = () => {
   return (
     <div>
-      <p>hej</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum corrupti
+        adipisci illum iure? Nihil iste minus quae, blanditiis nisi fuga iure
+        nam maxime laudantium tempore ea quasi ducimus alias. Debitis quisquam
+        quia velit ratione quae. Deserunt error veniam culpa esse dolore,
+        dignissimos eum quia quidem accusantium, ut aut voluptatibus assumenda
+        illo velit. Repellendus suscipit voluptatum, blanditiis iusto quae, quos
+        maxime porro mollitia corporis odio voluptates autem fugit non enim
+        illum! Maiores iste aliquam beatae, qui fuga mollitia officia amet,
+        maxime harum ipsam sed magnam laboriosam corrupti. Accusamus quia
+        voluptatem doloribus nobis, vitae provident alias cum quas animi
+        cupiditate iusto atque?
+      </p>
     </div>
   );
 };
 
-export default Portfolio;
+export default Home;

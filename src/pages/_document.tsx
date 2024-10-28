2b22ce49a9e3c97b4f516a80d0e76d415de0692e
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2024-10-28 19:09:44 +0100

    mad some style changes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Tobias Olausson Portfolio</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
